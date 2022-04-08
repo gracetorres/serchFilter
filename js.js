abdb015a75224f8c1a4256f773698cb5a1c1cd34
Julio <juliorosselltapia@gmail.com>
2022-04-08 16:52:48 -0500

    serchFilter

---
+++ js.js
@@ -0,0 +1,403 @@
+const listaGeneral = [
+    {
+        imagen: "https://www.sercoplus.com/28702-home_default/refrigeracion-liquida-silverstone-pf120.jpg",
+        nombre: "REFRIGERACIÓN LIQUIDA SILVERSTONE PF120 ARGB",
+        precioDolar: 94.00,
+        precioSoles:  353.44,
+        stock: 10,
+        codigo: "111630079",
+        relevancia: 1,
+        bestSeller: 20
+    },
+    {
+        imagen: "https://www.sercoplus.com/29710-home_default/cooler-de-procesador-antryx-mirage-210.jpg",
+        nombre: "COOLER DE PROCESADOR ANTRYX MIRAGE 210, RGB RAINBOW",
+        precioDolar: 18.00,
+        precioSoles:  67.68,
+        stock: 20,
+        codigo: "118900065",
+        relevancia: 2,
+        bestSeller: 20
+    },
+    {
+        imagen: "https://www.sercoplus.com/22302-home_default/procesador-intel-core-i5.jpg",
+        nombre: "PROCESADOR INTEL CORE I5-10400",
+        precioDolar: 198.00,
+        precioSoles: 744.48,
+        stock: 20,
+        codigo: "110366004",
+        relevancia: 3,
+        bestSeller: 18
+    },
+    {
+        imagen: "https://www.sercoplus.com/21614-home_default/refrigeracion-liquida-gigabyte-aorus-360.jpg",
+        nombre: "REFRIGERACIÓN LÍQUIDA GIGABYTE AORUS 360 RGB",
+        precioDolar: 264.00,
+        precioSoles:  992.64,
+        stock: 1,
+        codigo: "111630053",
+        relevancia: 4,
+        bestSeller: 20
+    },
+    {
+        imagen: "https://www.sercoplus.com/22595-home_default/refrigeracion-liquida-deepcool-castle-24.jpg",
+        nombre: "REFRIGERACIÓN LÍQUIDA DEEPCOOL CASTLE 240 V2 RGB",
+        precioDolar: 140.00,
+        precioSoles: 526.40,
+        stock: 17,
+        codigo: "111630075",
+        relevancia: 5,
+        bestSeller: 18
+    },
+    {
+        imagen: "https://www.sercoplus.com/24061-home_default/cooler-procesador-deepcool-gammaxx-gt-ar.jpg",
+        nombre: "COOLER PROCESADOR DEEPCOOL GAMMAXX GT ARGB",
+        precioDolar: 50.00,
+        precioSoles: 188.00,
+        stock: 6,
+        codigo: "118900046",
+        relevancia: 6,
+        bestSeller: 17
+    },
+    {
+        imagen: "https://www.sercoplus.com/27235-home_default/cooler-procesador-teros-te-8160n-led.jpg",
+        nombre: "COOLER PROCESADOR TEROS TE-8160N LED",
+        precioDolar: 9.00,
+        precioSoles:  33.84,
+        stock: 1,
+        codigo: "118900053",
+        relevancia: 7,
+        bestSeller: 17
+    },
+    {
+        imagen: "https://www.sercoplus.com/28686-home_default/refrigeracion-liquida-silverstone-pf240.jpg",
+        nombre: "REFRIGERACIÓN LIQUIDA SILVERSTONE PF240 ARGB",
+        precioDolar: 112.00,
+        precioSoles:  421.12,
+        stock: 4,
+        codigo: "111630080",
+        relevancia: 8,
+        bestSeller: 16
+    },
+    {
+        imagen: "https://www.sercoplus.com/29700-home_default/cooler-de-procesador-antryx-mirage-510.jpg",
+        nombre: "COOLER DE PROCESADOR ANTRYX MIRAGE 510, ARGB",
+        precioDolar:  38.00,
+        precioSoles:  142.88,
+        stock: 4,
+        codigo: "118900066",
+        relevancia: 9,
+        bestSeller: 15
+    },
+    {
+        imagen: "https://www.sercoplus.com/15755-home_default/refirgeracion-liquida-antryx-triton-120c.jpg",
+        nombre: "REFRIGERACIÓN LIQUIDA ANTRYX TRITON 120 ARGB.",
+        precioDolar:  60.00,
+        precioSoles: 225.60,
+        stock: 20,
+        codigo: "111630039",
+        relevancia: 10,
+        bestSeller: 14
+    },
+    {
+        imagen: "https://www.sercoplus.com/18881-home_default/procesador-amr-ryzen-9-5900x.jpg",
+        nombre: "PROCESADOR AMD RYZEN 9 5900X",
+        precioDolar: 646.00,
+        precioSoles:  2428.96,
+        stock: 7,
+        codigo: "110269004",
+        relevancia: 1,
+        bestSeller: 13
+    },
+    {
+        imagen: "https://www.sercoplus.com/22129-home_default/refrigeracion-liquida-nzxt-kraken-x63-rg.jpg",
+        nombre: "REFRIGERACIÓN LIQUIDA NZXT KRAKEN X63 RGB",
+        precioDolar: 190.00,
+        precioSoles:  706.80,
+        stock: 1,
+        codigo: "111630068",
+        relevancia: 2,
+        bestSeller: 12
+    },
+    {
+        imagen: "https://www.sercoplus.com/22887-home_default/procesador-intel-celeron-g5905.jpg",
+        nombre: "PROCESADOR INTEL CELERON G5905",
+        precioDolar: 53.00,
+        precioSoles: 197.16,
+        stock: 20,
+        codigo: "110366018",
+        relevancia: 3,
+        bestSeller: 11
+    },
+    {
+        imagen: "https://www.sercoplus.com/25280-home_default/procesador-amd-ryzen-3-pro-4350g.jpg",
+        nombre: "PROCESADOR AMD RYZEN 3 PRO 4350G OEM",
+        precioDolar: 198.00,
+        precioSoles: 736.56,
+        stock: 20,
+        codigo: "110272001",
+        relevancia: 4,
+        bestSeller: 10
+    },
+    {
+        imagen: "https://www.sercoplus.com/27874-home_default/cooler-procesador-deepcool-gammax-300.jpg",
+        nombre: "COOLER PROCESADOR DEEPCOOL GAMMAX 300",
+        precioDolar: 23.00,
+        precioSoles: 85.56,
+        stock: 10,
+        codigo: "118900055",
+        relevancia: 5,
+        bestSeller: 9
+    },
+    {
+        imagen: "https://www.sercoplus.com/28712-home_default/procesador-intel-core-i7-12700f.jpg",
+        nombre: "PROCESADOR INTEL CORE i7-12700F",
+        precioDolar: 414.00,
+        precioSoles:  1540.08,
+        stock: 20,
+        codigo: "110373009",
+        relevancia: 6,
+        bestSeller: 8
+    },
+    {
+        imagen: "https://www.sercoplus.com/29720-home_default/procesador-intel-core-i7-12700.jpg",
+        nombre: "PROCESADOR INTEL CORE I7-12700",
+        precioDolar: 442.00,
+        precioSoles:  1644.24,
+        stock: 20,
+        codigo: "110373012",
+        relevancia: 7,
+        bestSeller: 20
+    },
+    {
+        imagen: "https://www.sercoplus.com/20248-home_default/evga-clc-120-rgb-led.jpg",
+        nombre: "PROCESADOR INTEL CORE I7-12700",
+        precioDolar:  82.00,
+        precioSoles: 305.04,
+        stock: 20,
+        codigo: "111630006",
+        relevancia: 8,
+        bestSeller: 6
+    },
+    {
+        imagen: "https://www.sercoplus.com/14342-home_default/procesador-amd-athlon-3000g-am4.jpg",
+        nombre: "PROCESADOR AMD ATHLON 3000G AM4.",
+        precioDolar: 92.00,
+        precioSoles: 342.24,
+        stock: 10,
+        codigo: "110259004",
+        relevancia: 9,
+        bestSeller: 20
+    },
+    {
+        imagen: "https://www.sercoplus.com/24057-home_default/cooler-procesador-deepcool-gammaxx-gt-tg.jpg",
+        nombre: "COOLER PROCESADOR DEEPCOOL GAMMAXX GT TGA RGB",
+        precioDolar: 54.00,
+        precioSoles:  200.88,
+        stock: 10,
+        codigo: "118900042",
+        relevancia: 10,
+        bestSeller: 5
+    },
+    {
+        imagen: "https://www.sercoplus.com/28147-home_default/procesador-amd-a8-9600-3-10ghz-2mb-l2.jpg",
+        nombre: "Procesador AMD A8-9600, 3.10GHz, 2MB L2",
+        precioDolar: 72.00,
+        precioSoles:267.84,
+        stock: 8,
+        codigo: "110253002",
+        relevancia: 1,
+        bestSeller: 3
+    },
+    {
+        imagen: "https://www.sercoplus.com/18716-home_default/procesador-amd-ryzen-7-5800x.jpg",
+        nombre: "PROCESADOR AMD RYZEN 7 5800X",
+        precioDolar:  444.00 ,
+        precioSoles: 1651.68,
+        stock: 14,
+        codigo: "110269002",
+        relevancia: 2,
+        bestSeller: 1
+    },
+    
+]
+
+let anotherArray= [];
+let appliedFilter = false;
+
+function funcionesIniciales(){
+    renderProductList(listaGeneral)
+}
+
+function renderProductList(productList) {
+    const container = document.getElementById("container");
+    let html = "";
+    for (let index = 0; index < productList.length; index++) {
+        html+=
+        `<div class="w25">
+            <div class="flex-column">
+                <img class="imagen" src="${productList[index].imagen}" alt="">
+                <div class="tituloProducto flex-row justify-center">${productList[index].nombre}</div>
+                <div class="flex-row align-center">
+                    <div class="flex-column mt5 p10">
+                        <label class="precios" for="">${productList[index].precioDolar}</label>
+                        <label class="precios" for="">${productList[index].precioSoles}</label>
+                        <label class="normal" for="">Stock <span>${productList[index].stock}</span></label>
+                        <label class="normal" for="">Ref <span>${productList[index].codigo}</span></label>
+                    </div>
+                    <button onclick="añadirACarrito(${index})" class="button">Comprar</button>
+                </div>
+            </div>
+        </div>`
+    } 
+    container.innerHTML = html; 
+}
+
+
+function filtrarByProperty(properyName){
+    appliedFilter = true;
+    anotherArray = [];
+    for (let index = 0; index < listaGeneral.length; index++) {
+        if (listaGeneral[index].nombre.includes(properyName)) {
+            anotherArray.push(listaGeneral[index])
+        }
+    } 
+    sortBy()
+
+}
+
+
+function sortByProperty(array,property, inverse = false ){
+    const arraySortered = array.sort(function (a, b) {
+        if (a[property] > b[property]) {
+            return 1;
+        }
+        if (a[property] < b[property]) {
+            return -1;
+        }
+     
+        return 0;
+    });
+
+    if(inverse) {
+        return arraySortered.reverse();
+    }
+    return arraySortered;
+}
+
+
+
+function sortBy(){
+    añadirDelay();
+    setTimeout(() => {
+        const selects = document.getElementById("select").value;
+        let arrayOrdenado = [];
+    
+        /**
+         * MV : mas vendidos
+         * RE : relevancia
+         * AZ : A-Z
+         * ZA: Z-A
+         * MN : MAYOR-MENOR
+         * NM : MENOR . MAYOR
+         * 
+         */
+        const arrayOrdenar = appliedFilter ? anotherArray : listaGeneral;
+        switch (selects) {
+            case "MV": 
+                arrayOrdenado = sortByProperty(arrayOrdenar,"bestSeller",true)
+               break;
+            case "RE":
+                arrayOrdenado = sortByProperty(arrayOrdenar,"revelancia",true)
+                break;
+            case "AZ":
+                arrayOrdenado = sortByProperty(arrayOrdenar,"nombre")
+                break;
+            case "ZA":
+                arrayOrdenado = sortByProperty(arrayOrdenar,"nombre",true)
+                break;
+            case "MN":
+                arrayOrdenado = sortByProperty(arrayOrdenar,"precioDolar",true)
+                break;
+            case "NM":
+                arrayOrdenado = sortByProperty(arrayOrdenar,"precioDolar")
+                break;
+                                
+            default:
+                arrayOrdenado = anotherArray;
+                break;
+        }
+     
+        renderProductList(arrayOrdenado)
+        // quitarDelay()
+    }, 2000);
+
+}
+
+ function añadirDelay(){
+    const container = document.getElementById("container");
+    const contdos = document.getElementById("contdos");
+
+    container.classList.add("display-none");
+    contdos.classList.remove("display-none");
+  
+   
+
+}
+
+function quitarDelay(){
+    const container = document.getElementById("container");
+    const contdos = document.getElementById("contdos");
+
+    container.classList.remove("display-none");
+    contdos.classList.add("display-none");
+}
+
+
+const productosEnCarrito = [];
+
+function añadirACarrito(id){
+    const productoCarro = document.getElementById("productoCarro");
+    let html = "";
+    
+    const ob = listaGeneral[id];
+    productosEnCarrito.push(ob);
+
+    console.log(productosEnCarrito)
+
+    for (let index = 0; index < productosEnCarrito.length; index++) {
+        html+=
+        `<div class="flex-row p10 align-center">
+            <img src="${productosEnCarrito[index].imagen}" alt=""> 
+            <div class="flex-column">
+                <label class="label" for="">${productosEnCarrito[index].nombre}</label>
+                <div class="mt10">
+                    <label class="precios" for=""> <span class="precios">S/</span> ${productosEnCarrito[index].precioSoles} </label>                
+                    <span class="label">x</span> 
+                    <label class="labelCantidad" for="">Cantidad</label>
+                </div>
+            </div>
+            <div class="flex-end w100 p10">
+            <button onclick="eliminarProductoCarrito(${index})" class="boton">Eliminar</button>
+        </div>
+        </div>
+        `
+    }
+    productoCarro.innerHTML = html;
+}
+
+function eliminarProductoCarrito(posicion){
+    productosEnCarrito.splice(posicion,1);
+    añadirACarrito(id);
+}
+
+function abrirModalCarrito(){
+    
+    const carrito = document.getElementById("carrito");
+    carrito.classList.remove("display-none");
+}
+
+function cerrarModalCarro(){
+    const carrito = document.getElementById("carrito");
+    carrito.classList.add("display-none");
+}
